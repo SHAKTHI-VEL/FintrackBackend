01ae2f16ef7825eea9f8eb14006ead17a7c493d6
SHAKTHI-VEL <shakthivel28203@gmail.com>
2022-10-12 19:23:07 +0530

    v 1.9.1

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,16 +1,13 @@
+const { response } = require('express');
 const Transaction =require('../models/Transaction')
 
 // @desc Get all transaction
 // @route GET /api/v1/transactions
 // @access PUBLIC
-exports.getTransactions = async (req, res, next) => {
+exports.getTransactions = async (req, res) => {
     try {
       const transactions = await Transaction.find({user:req.user.id});
-      return res.status(200).json({
-        success: true,
-        count: transactions.length,
-        data: transactions
-      });
+     res.send(transactions)
     } catch (err) {
       return res.status(500).json({
         success: false,
